a8b350a1c17dc97805f63537dbb3393708183eb1
FilevIllya <velikifilin@gmail.com>
2023-09-09 22:16:23 +0300

    Cycles

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -6,6 +6,172 @@ namespace B_ControlFlow
     {
         static void Main(string[] args)
         {
+            int[] numberis = { 0, 3, 2, 1, 5, 4, 6, 7, 8, 9 };
+
+            foreach (int n in numberis)
+            {
+                //if (n%2==0)
+                //    Console.WriteLine(n);
+                //OR
+
+                if (n % 2 != 0)
+                    continue; //next iteration
+
+                Console.WriteLine(n);
+            }
+
+            Console.ReadLine();
+
+            char[] letters = {'a','b','c','d','e','f','g','h','i','j'};
+
+            for (int i = 0; i < numberis.Length; i++)
+            {
+                Console.WriteLine($"Number={numberis[i]}");
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (numberis[i] == j)
+                        break;
+                    Console.Write($" {letters[j]}");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+
+            int[] numbers = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            int counter = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (counter == 3)
+                    break;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair ({atI};{atJ}). Indexes ({i};{j})");
+                        counter++; 
+                    }
+                    if (counter==3)
+                        break;
+                }
+            }
+        }
+
+        static void WhileDoWhile()
+        {
+            int age = 30;
+
+            //there first condition
+            while (age < 18)
+            {
+                Console.WriteLine("First while loop");
+                Console.WriteLine("What is your age");
+                age = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Good)");
+
+            //there first cycle
+            do
+            {
+                Console.WriteLine("Do/While");
+                Console.WriteLine("What is your age");
+                age = int.Parse(Console.ReadLine());
+            } while (age < 18);
+
+            //while like for:
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            int i = 0;
+            while (i < numbers.Length)
+            {
+                Console.Write(numbers[i] + " ");
+                i++;
+            }
+        }
+        static void NestedFor()
+        {
+            int[] numbers = { 1, -2, 4, -7, 5, 3, 2, -1, -3, 2, 7, -1, -3, 1, 7 };
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Pair ({atI};{atJ}). Indexes ({i};{j})");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length - 2; i++)
+            {
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < numbers.Length; k++)
+                    {
+                        int atI = numbers[i];
+                        int atJ = numbers[j];
+                        int atK = numbers[k];
+
+                        if (atI + atJ + atK == 0)
+                        {
+                            Console.WriteLine($"Triplets ({atI};{atJ};{atK}). Indexes ({i};{j};{k})");
+                        }
+                    }
+
+                }
+            }
+        }
+        static void Forforeach()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //              ->>>>>>
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            //             <<<<<<<-
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Foreach");
+
+            //only for each variable in array
+            foreach (int val in numbers)
+            {
+                Console.Write(val + " ");
+            }
+        }
+        static void IfMaxNumber()
+        {
             Console.WriteLine("Enter two numbers");
             Console.Write("Number 1: ");
             double num1 = Double.Parse(Console.ReadLine());
@@ -20,7 +186,6 @@ namespace B_ControlFlow
                 Console.WriteLine($"Number {num2} is bigger then {num1}");
             }
         }
-
         static void IfElse()
         {
             Console.Write("Enter your age: ");
