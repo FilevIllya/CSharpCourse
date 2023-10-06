5326d9f99ff7c600472e49456196fc159639e0c9
FilevIllya <velikifilin@gmail.com>
2023-10-06 21:03:35 +0300

    Complex Number Class

---
+++ F_PracticSeection/Complex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F_PracticSeection
+{
+    class Complex
+    {
+        public double RealPart { get; private set; }
+        public double ImaginaryPart { get; private set; }
+
+        public Complex(double realPart, double imaginaryPart)
+        {
+            this.RealPart = realPart;
+            this.ImaginaryPart = imaginaryPart;
+        }
+
+        public Complex ComplexAdditions(Complex secondComplex)
+        {
+            Complex result = new Complex(RealPart + secondComplex.RealPart, ImaginaryPart + secondComplex.ImaginaryPart);
+            return result;
+        }
+
+        public Complex ComplexSubtraction(Complex secondComplex)
+        {
+            Complex result = new Complex(RealPart - secondComplex.RealPart, ImaginaryPart - secondComplex.ImaginaryPart);
+            return result;
+        }
+    }
+}
---
+++ F_PracticSeection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace F_PracticSeection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 2);
+            Complex c2 = new Complex(3, 1);
+
+            Complex result = c1.ComplexSubtraction(c2);
+            Console.WriteLine($"Your complex number is {result.RealPart}+{result.ImaginaryPart}i");
+        }
+    }
+}
